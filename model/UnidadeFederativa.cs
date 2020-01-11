c899bb09a193dada328d4ea18dfbde122bc85fa9
Michael Santos <michael.shel96@gmail.com>
2020-01-11 20:26:29 -0300

    Definição da class enum para unidade federativa.

---
+++ model/UnidadeFederativa.cs
@@ -0,0 +1,33 @@
+using System;
+
+public enum UnidadeFederativa {
+
+    AC,
+    AL,
+    AP,
+    AM,
+    BA,
+    CE,
+    DF,
+    ES,
+    GO,
+    MA,
+    MT,
+    MS,
+    MG,
+    PA,
+    PB,
+    PR,
+    PE,
+    PI,
+    RJ,
+    RN,
+    RS,
+    RO,
+    RR,
+    SC,
+    SP,
+    SE,
+    TO
+
+}
