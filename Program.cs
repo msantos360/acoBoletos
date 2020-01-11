a201244e82bfa96e20cd51727f426781faa11cb0
Michael Santos <michael.shel96@gmail.com>
2020-01-11 19:06:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace acoBoletos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
