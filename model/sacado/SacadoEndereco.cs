1f52246f0b64cdf5e5deafee8a2e5333d5b0bd1f
Michael Santos <michael.shel96@gmail.com>
2020-01-11 20:37:21 -0300

    Definição da class de endereço do sacado.

---
+++ model/sacado/SacadoEndereco.cs
@@ -0,0 +1,34 @@
+using System;
+
+class SacadoEndereco {
+    
+    public string logradouro { get; }
+
+    public int numero { get; }
+
+    public string bairro { get; }
+
+    public string localidade { get; }
+
+    public UnidadeFederativa uf { get; }
+
+    public string cep { get; }
+
+    public SacadoEndereco(string logradouro,
+                          int numero,
+                          string bairro,
+                          string localidade,
+                          UnidadeFederativa uf,
+                          string cep)
+    {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+        this.cep = cep;
+    }
+
+
+
+}
