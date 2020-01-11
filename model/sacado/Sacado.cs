2349b53c2fe88096a20dec031024c02837c54670
Michael Santos <michael.shel96@gmail.com>
2020-01-11 20:36:38 -0300

    Definição da class Sacado

---
+++ model/sacado/Sacado.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Sacado {
+    
+    public string nome { get; }
+
+    public string identy { get; } //CPF ou CNPJ
+
+    public SacadoEndereco endereco { get; }
+
+    public Sacado(string nome,
+                  string identy,
+                  SacadoEndereco endereco)
+    {
+        this.nome = nome;
+        this.identy = identy;
+        this.endereco = endereco;
+    }
+
+
+
+
+}
