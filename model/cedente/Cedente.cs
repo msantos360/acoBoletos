bfea0c9eaa823ad01d21a493f51d7eefbefd71b9
Michael Santos <michael.shel96@gmail.com>
2020-01-11 21:38:55 -0300

    Definição da class Cedente com implementação das class Inscrição e ContaCorrente.

---
+++ model/cedente/Cedente.cs
@@ -0,0 +1,30 @@
+using System;
+class Cedente {
+    
+    public string nome { get; } //Nome da Empresa
+
+    public string cnpj { get; }
+
+    public Inscricao incricao { get; }
+
+    public ContaCorrente contaCorrente { get; }
+
+    public string cnab { get; } //Uso Exclusivo da FEBRABAN/CNAB
+
+    public Cedente(string nome,
+                   string cnpj,
+                   Inscricao inscricao,
+                   ContaCorrente contaCorrente,
+                   string cnab
+                   )
+    {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.incricao = inscricao;
+        this.contaCorrente = contaCorrente;
+        this.cnab = cnab;
+    }
+
+
+
+}
