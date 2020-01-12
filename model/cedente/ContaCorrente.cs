77eb9f375d8ccf1d9c0276e3ebcdc4d8531439c4
Michael Santos <michael.shel96@gmail.com>
2020-01-11 21:38:01 -0300

    Definição da class ContaCorrente para o cedente.

---
+++ model/cedente/ContaCorrente.cs
@@ -0,0 +1,30 @@
+using System;
+public class ContaCorrente {
+
+    public int agencia { get; } //Agência Mantenedora da Conta
+
+    public string dgAgencia { get; } //Dígito Verificador da Agência
+
+    public int conta { get; } //Número da Conta Corrente
+
+    public string dgConta { get; }  //Dígito Verificador da Conta
+
+    public string dgAgConta { get; } //Dígito Verificador da Ag/Conta
+
+    public ContaCorrente(int agencia,
+                         string dgAgencia,
+                         int conta,
+                         string dgConta,
+                         string dgAgConta
+                        )
+    {
+        this.agencia = agencia;
+        this.dgAgencia = dgAgencia;
+        this.conta = conta;
+        this.dgConta = dgConta;
+        this.dgAgConta = dgAgConta;
+    }
+
+
+
+}
