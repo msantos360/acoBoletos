2077d35d3c4a58cfd8f206716f619c9995500862
Michael Santos <michael.shel96@gmail.com>
2020-01-13 22:07:05 -0300

    Testes de desenvolvimento.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace acoBoletos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SacadoEndereco s1 = new SacadoEndereco(
+                "null",
+                50,
+                "Jd. das pitangas",
+                "Guarulhos",
+                UnidadeFederativa.SP,
+                "07245-056");
+
+            Sacado paulo = new Sacado("Paulo","3710780284466",s1);
+
+
+
         }
     }
 }
